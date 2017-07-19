f4374417bfa5db6a7ff43c62c1cee15ae90205a6
Nezar <nabdennur@gmail.com>
2017-07-19 05:29:21 -0400

    More exports

--- bioframe/__init__.py
+++ bioframe/__init__.py
@@ -1,9 +1,10 @@
 #-*- coding: utf-8 -*-
 from __future__ import division, print_function
-__version__ = '0.1.0-alpha'
+__version__ = '0.0.1'
 
 from . import io
 from . import tools
 from .io.formats import *
 from .io.resources import *
+from .core import *
 from .schemas import SCHEMAS
