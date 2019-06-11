a832de73f5306c464af1b03f4eaea0081c0dc57c
Nezar <nabdennur@gmail.com>
2019-06-11 13:34:23 -0400

    Add parquet reader

--- bioframe/io/formats.py
+++ bioframe/io/formats.py
@@ -549,3 +549,61 @@ def to_parquet(pieces, outpath, row_group_size=None, compression='snappy',
             writer.write_table(table, row_group_size=row_group_size)
     finally:
         writer.close()
+
+
+def read_parquet(filepath, columns=None, iterator=False, **kwargs):
+    """
+    Load DataFrames from Parquet files, optionally in pieces.
+
+    Parameters
+    ----------
+    filepath : str, pathlib.Path, pyarrow.NativeFile, or file-like object
+        Readable source. For passing bytes or buffer-like file containing a
+        Parquet file, use pyarorw.BufferReader
+    columns: list
+        If not None, only these columns will be read from the row groups. A
+        column name may be a prefix of a nested field, e.g. 'a' will select
+        'a.b', 'a.c', and 'a.d.e'
+    iterator : boolean, default False
+        Return an iterator object that yields row group DataFrames and
+        provides the ParquetFile interface.
+    use_threads : boolean, default True
+        Perform multi-threaded column reads
+    memory_map : boolean, default True
+        If the source is a file path, use a memory map to read file, which can
+        improve performance in some environments
+
+    Returns
+    -------
+    DataFrame or ParquetFileIterator
+
+    """
+    if not iterator:
+        return pd.read_parquet(filepath, columns=columns,
+                               use_threads=use_threads, **kwargs)
+    else:
+        try:
+            from pyarrow.parquet import ParquetFile
+        except ImportError:
+            raise ImportError(
+                'Iterating over Parquet data requires the `pyarrow` package.')
+
+        use_threads = kwargs.pop('use_threads', True)
+
+        class ParquetFileIterator(ParquetFile):
+            def __iter__(self):
+                return self
+            def __next__(self):
+                if not hasattr(self, '_rgid'):
+                    self._rgid = 0
+                if self._rgid < self.num_row_groups:
+                    rg = self.read_row_group(
+                        self._rgid,
+                        columns=columns,
+                        use_threads=use_threads,
+                        use_pandas_metadata=True)
+                    self._rgid += 1
+                else:
+                    raise StopIteration
+                return rg.to_pandas()
+        return ParquetFileIterator(filepath, **kwargs)
