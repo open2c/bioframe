0600e26482fb500a731ce8e1153e0065ed136d3b
gfudenberg <geoff.fudenberg@gmail.com>
2020-04-26 14:48:25 -0700

    test-overlap

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,6 +1,35 @@
 import pandas as pd
 import bioframe
+import pyranges as pr
+import numpy as np
 
+def bioframe_to_pyranges(df):
+    pydf = df.copy()
+    pydf.rename({'chrom':'Chromosome','start':'Start','end':'End'},
+                  axis='columns', inplace=True)
+    return pr.PyRanges(pydf)
+
+def pyranges_overlap_to_bioframe(pydf):
+    ## convert the df output by pyranges join into a bioframe-compatible format
+    df = pydf.df.copy()
+    df.rename({'Chromosome':'chrom_1','Start':'start_1','End':'end_1',
+               'Start_b':'start_2','End_b':'end_2'},
+                  axis='columns', inplace=True)
+    df['chrom_1'] = df['chrom_1'].values.astype('object') #to remove categories
+    df['chrom_2'] = df['chrom_1'].values    
+    return df
+
+chroms = ['chr12','chrX']
+def mock_bioframe(num_entries =100):
+    pos = np.random.randint(1, 1e7, size=(num_entries,2))
+    df = pd.DataFrame()
+    df['chrom'] = np.random.choice(chroms,num_entries)
+    df['start'] = np.min(pos,axis=1)
+    df['end'] = np.max(pos,axis=1)
+    df.sort_values(['chrom','start'],inplace=True)
+    return df
+
+############# tests #####################
 
 def test_expand():
     fake_bioframe = pd.DataFrame(
@@ -20,3 +49,20 @@ def test_expand():
     assert (
         fake_expanded.iloc[2].start == fake_bioframe.iloc[2].start - expand_bp
     )  # expand start normally
+
+
+def test_overlap():
+    ### note does not test overlap_start or overlap_end columns of bioframe.overlap
+    df1 = mock_bioframe() 
+    df2 = mock_bioframe() 
+    assert (df1.equals(df2)==False)
+    p1 = bioframe_to_pyranges(df1)
+    p2 = bioframe_to_pyranges(df2)
+    pp = pyranges_overlap_to_bioframe(p1.join(p2))[['chrom_1','start_1','end_1','chrom_2','start_2','end_2']]
+    bb = bioframe.overlap(df1,df2)[['chrom_1','start_1','end_1','chrom_2','start_2','end_2']]
+    pd.testing.assert_frame_equal(bb,pp,check_dtype=False, check_exact=True)
+    print('overlap elements agree')
+
+
+
+
