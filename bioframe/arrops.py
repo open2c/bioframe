334cc5c82977572bf55801abac08987fda8c4fac
Anton Goloborodko <goloborodko.anton@gmail.com>
2020-06-23 20:23:37 +0200

    arrops: ignore self-overlaps in overlap_arrays, drop sorting

--- bioframe/arrops.py
+++ bioframe/arrops.py
@@ -137,7 +137,7 @@ def _size_overlap(starts1, ends1, starts2, ends2):
     return overlap_size
 
 
-def overlap_intervals(starts1, ends1, starts2, ends2, closed=False):
+def _overlap_intervals_legacy(starts1, ends1, starts2, ends2, closed=False, sort=False):
     """
     Take two sets of intervals and return the indices of pairs of overlapping intervals.
     
@@ -214,7 +214,90 @@ def overlap_intervals(starts1, ends1, starts2, ends2, closed=False):
     overlap_ids[:, 1] = overlap_ids[:, 1] - 1
 
     # Sort overlaps according to the 1st
-    overlap_ids = overlap_ids[np.lexsort([overlap_ids[:, 1], overlap_ids[:, 0]])]
+    if sort:
+        overlap_ids = overlap_ids[np.lexsort([overlap_ids[:, 1], overlap_ids[:, 0]])]
+
+    return overlap_ids
+
+
+def overlap_intervals(starts1, ends1, starts2, ends2, closed=False, sort=False):
+    """
+    Take two sets of intervals and return the indices of pairs of overlapping intervals.
+    
+    Parameters
+    ----------
+    starts1, ends1, starts2, ends2 : numpy.ndarray
+        Interval coordinates. Warning: if provided as pandas.Series, indices
+        will be ignored.
+        
+    closed : bool
+        If True, then treat intervals as closed and report single-point overlaps.
+    Returns
+    -------
+    overlap_ids : numpy.ndarray
+        An Nx2 array containing the indices of pairs of overlapping intervals.
+        The 1st column contains ids from the 1st set, the 2nd column has ids 
+        from the 2nd set.
+    
+    """
+
+    for vec in [starts1, ends1, starts2, ends2]:
+        if issubclass(type(vec), pd.core.series.Series):
+            warnings.warn(
+                "One of the inputs is provided as pandas.Series and its index "
+                "will be ignored.",
+                SyntaxWarning,
+            )
+
+    starts1 = np.asarray(starts1)
+    ends1 = np.asarray(ends1)
+    starts2 = np.asarray(starts2)
+    ends2 = np.asarray(ends2)
+
+    # Concatenate intervals lists
+    n1 = len(starts1)
+    n2 = len(starts2)
+    ids1 = np.arange(0,n1)
+    ids2 = np.arange(0,n2)
+
+    # Sort all intervals together
+    order1 = np.lexsort([ends1, starts1])
+    order2 = np.lexsort([ends2, starts2])
+    starts1, ends1, ids1 = starts1[order1], ends1[order1], ids1[order1]
+    starts2, ends2, ids2 = starts2[order2], ends2[order2], ids2[order2]
+    
+    # Find interval overlaps
+    match_2in1_starts = np.searchsorted(starts2, starts1, "left")
+    match_2in1_ends = np.searchsorted(starts2, ends1, "right" if closed else "left")
+    # "right" is intentional here to avoid duplication
+    match_1in2_starts = np.searchsorted(starts1, starts2, "right") 
+    match_1in2_ends = np.searchsorted(starts1, ends2, "right" if closed else "left")
+    
+    # Ignore self-overlaps
+    match_2in1_mask = match_2in1_ends > match_2in1_starts 
+    match_1in2_mask = match_1in2_ends > match_1in2_starts 
+    match_2in1_starts, match_2in1_ends = (
+        match_2in1_starts[match_2in1_mask], match_2in1_ends[match_2in1_mask])
+    match_1in2_starts, match_1in2_ends = (
+        match_1in2_starts[match_1in2_mask], match_1in2_ends[match_1in2_mask])
+
+    # Generate IDs of pairs of overlapping intervals
+    overlap_ids = np.block(
+    [
+        [
+            np.repeat(ids1[match_2in1_mask], match_2in1_ends - match_2in1_starts)[:,None],
+            ids2[arange_multi(match_2in1_starts, match_2in1_ends)][:,None],
+        ],
+        [
+            ids1[arange_multi(match_1in2_starts, match_1in2_ends)][:,None],
+            np.repeat(ids2[match_1in2_mask], match_1in2_ends - match_1in2_starts)[:,None],
+        ]
+    ]
+    )
+    
+    if sort:
+        # Sort overlaps according to the 1st
+        overlap_ids = overlap_ids[np.lexsort([overlap_ids[:, 1], overlap_ids[:, 0]])]
 
     return overlap_ids
 
