92547ad543d62fc80685d1a2d0cf59f704cea5c3
Nezar Abdennur <nabdennur@gmail.com>
2023-05-28 01:37:47 -0400

    docs: Update docs on local storage resources

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath("../"))
 
-autodoc_mock_imports = ["numpy", "pandas", "matplotlib", "requests"]
+# autodoc_mock_imports = ["numpy", "pandas", "matplotlib", "requests"]
 
 
 # -- Project information -----------------------------------------------------
