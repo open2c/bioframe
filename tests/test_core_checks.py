4c88af9649fcb1fbf7c7cda80eb58a7b6046c5ff
smitkadvani <smit.kadvani@gmail.com>
2024-01-19 11:42:33 -0800

    linting errors resolved in test file

--- tests/test_core_checks.py
+++ tests/test_core_checks.py
@@ -18,7 +18,7 @@ from bioframe.ops import sort_bedframe
 
 
 def test_is_cataloged():
-    ### chr2q is not in view
+    #   chr2q is not in view
     view_df = pd.DataFrame(
         [
             ["chr1", 0, 12, "chr1p"],
@@ -38,7 +38,7 @@ def test_is_cataloged():
     )
     assert not is_cataloged(df, view_df)
 
-    ### chr1q is in view, df_view_col and view_name_col have funny labels.
+    #    chr1q is in view, df_view_col and view_name_col have funny labels.
     view_df = pd.DataFrame(
         [
             ["chr1", 0, 12, "chr1p"],
@@ -56,7 +56,10 @@ def test_is_cataloged():
         columns=["chrom", "start", "end", "funny_view_region"],
     )
     assert is_cataloged(
-        df, view_df, df_view_col="funny_view_region", view_name_col="funny_name"
+        df,
+        view_df,
+        df_view_col="funny_view_region",
+        view_name_col="funny_name"
     )
 
 
@@ -71,7 +74,7 @@ def test_is_contained():
         columns=["chrom", "start", "end", "name"],
     )
 
-    ### not contained because chr2q is not cataloged
+    #   not contained because chr2q is not cataloged
     df = pd.DataFrame(
         [
             ["chr1", 0, 12, "chr1p"],
@@ -82,7 +85,7 @@ def test_is_contained():
     )
     assert not is_contained(df, view_df, df_view_col="view_region")
 
-    ### not contained because second interval falls outside the view regions
+    #   not contained because second interval falls outside the view regions
     df = pd.DataFrame(
         [
             ["chr1", 14, 15, "chr1p"],
@@ -103,8 +106,9 @@ def test_is_contained():
     )
     # is contained, because assignments are inferred
     assert is_contained(df, view_df)
-    
-    # using previous assignments with non-standard column names in dataframes without argument
+
+    # using previous assignments with non-standard column names
+    # in dataframes without argument
     view_df = pd.DataFrame(
         [
             ["chr1", 0, 12, "chr1p"],
@@ -121,7 +125,14 @@ def test_is_contained():
         ],
         columns=["chrom1", "start1", "end1", "VIEW_REGION"],
     )
-    assert is_contained(df, view_df, cols=["chrom1", "start1", "end1"], cols_view=["CHROM", "START", "END"], df_view_col="VIEW_REGION", view_name_col="NAME")
+    assert is_contained(
+        df,
+        view_df,
+        cols=["chrom1", "start1", "end1"],
+        cols_view=["CHROM", "START", "END"],
+        df_view_col="VIEW_REGION",
+        view_name_col="NAME"
+        )
 
     with pytest.raises(TypeError):
         # cols and view_cols are not passed as an arguments
@@ -138,7 +149,7 @@ def test_is_contained():
     )
     assert not is_contained(df, view_df, df_view_col="view_region")
 
-    ### second interval falls completely out of the view
+    #   second interval falls completely out of the view
     df = pd.DataFrame(
         [
             ["chr1", 12, 12, "chr1p"],
@@ -156,7 +167,7 @@ def test_is_contained():
 
 
 def test_is_overlapping():
-    ### interval on chr1 overlaps
+    #   interval on chr1 overlaps
     d = """chrom  start  end
          0  chr1      3    6
          1  chr1     5   10
@@ -164,7 +175,7 @@ def test_is_overlapping():
     df = pd.read_csv(StringIO(d), sep=r"\s+")
     assert is_overlapping(df)
 
-    ### adjacent intervals do not overlap
+    #   adjacent intervals do not overlap
     d = """chrom  start  end
          0  chr1    3     6
          1  chr1    6    10
@@ -174,8 +185,8 @@ def test_is_overlapping():
 
 
 def test_is_covering():
-    ### test is_covering where an interval from df completely overlaps
-    ### two different regions from view
+    #   test is_covering where an interval from df completely overlaps
+    #   two different regions from view
     df1 = pd.DataFrame(
         [
             ["chr1", -5, 25],
@@ -185,8 +196,8 @@ def test_is_covering():
     chromsizes = [("chr1", 0, 9, "chr1p"), ("chr1", 11, 20, "chr1q")]
     assert is_covering(df1, chromsizes)
 
-    ### test is_covering where two intervals from df overlap
-    ### two different regions from view
+    #   test is_covering where two intervals from df overlap
+    #   two different regions from view
     df1 = pd.DataFrame(
         [
             ["chr1", -5, 10],
@@ -198,7 +209,7 @@ def test_is_covering():
     chromsizes = [("chr1", 0, 9, "chr1p"), ("chr1", 11, 20, "chr1q")]
     assert is_covering(df1, chromsizes)
 
-    ### test is_covering with non-standard columns names 
+    #   test is_covering with non-standard columns names
     df1 = pd.DataFrame(
         [
             ["chr1", -5, 10],
@@ -209,19 +220,25 @@ def test_is_covering():
     )
     chromsizes = pd.DataFrame(
         [
-            ["chr1", 0, 9, "chr1p"], 
+            ["chr1", 0, 9, "chr1p"],
             ["chr1", 11, 20, "chr1q"]
         ],
         columns=["CHROM", "START", "END", "NAME"],
         )
-    assert is_covering(df1, chromsizes, cols=["chrom1", "start1", "end1"], cols_view=["CHROM", "START", "END"], view_name_col="NAME")
+    assert is_covering(
+                        df1,
+                        chromsizes,
+                        cols=["chrom1", "start1", "end1"],
+                        cols_view=["CHROM", "START", "END"],
+                        view_name_col="NAME"
+                    )
 
     with pytest.raises(ValueError):
         # cols and view_cols are not passed as an arguments
         is_covering(df1, chromsizes)
 
-    ### test is_covering where two intervals from df overlap
-    ### two different regions from view
+    #   test is_covering where two intervals from df overlap
+    #   two different regions from view
     df1 = pd.DataFrame(
         [
             ["chr1", -5, 10, "chr1q"],
@@ -235,7 +252,7 @@ def test_is_covering():
 
 
 def test_is_tiling():
-    ### view region chr1p is tiled by one interval, chr1q is tiled by two
+    #   view region chr1p is tiled by one interval, chr1q is tiled by two
     df1 = pd.DataFrame(
         [
             ["chr1", 0, 9, "chr1p"],
@@ -247,7 +264,7 @@ def test_is_tiling():
     chromsizes = [("chr1", 0, 9, "chr1p"), ("chr1", 11, 20, "chr1q")]
     assert is_tiling(df1, chromsizes)
 
-    ### testing for non-standard column names
+    #   testing for non-standard column names
     df1 = pd.DataFrame(
         [
             ["chr1", 0, 9, "chr1p"],
@@ -258,20 +275,24 @@ def test_is_tiling():
     )
     chromsizes = pd.DataFrame(
         [
-            ["chr1", 0, 9, "chr1p"], 
+            ["chr1", 0, 9, "chr1p"],
             ["chr1", 11, 20, "chr1q"]
         ],
         columns=["CHROM", "START", "END", "NAME"],
         )
-    assert is_tiling(df1, chromsizes, cols=["chrom1", "start1", "end1"], cols_view=["CHROM", "START", "END"], view_name_col="NAME")
+    assert is_tiling(
+                        df1,
+                        chromsizes,
+                        cols=["chrom1", "start1", "end1"],
+                        cols_view=["CHROM", "START", "END"],
+                        view_name_col="NAME"
+                    )
 
     with pytest.raises(KeyError):
         # cols and view_cols are not passed as an arguments
         is_tiling(df1, chromsizes)
 
-
-
-    ### not tiling, since (chr1,0,9) is associated with chr1q
+    #  not tiling, since (chr1,0,9) is associated with chr1q
     df1 = pd.DataFrame(
         [
             ["chr1", 0, 9, "chr1q"],
@@ -283,7 +304,7 @@ def test_is_tiling():
     chromsizes = [("chr1", 0, 9, "chr1p"), ("chr1", 11, 20, "chr1q")]
     assert not is_tiling(df1, chromsizes)
 
-    ### not tiling, contains overlaps
+    #   not tiling, contains overlaps
     df1 = pd.DataFrame(
         [
             ["chr1", 0, 9, "chr1p"],
@@ -295,7 +316,7 @@ def test_is_tiling():
     chromsizes = [("chr1", 0, 9, "chr1p"), ("chr1", 11, 20, "chr1q")]
     assert not is_tiling(df1, chromsizes)
 
-    ### not tiling, since it doesn't cover
+    #   not tiling, since it doesn't cover
     df1 = pd.DataFrame(
         [
             ["chr1", 11, 12, "chr1q"],
@@ -308,7 +329,7 @@ def test_is_tiling():
 
 
 def test_is_bedframe():
-    ##missing a column
+    #   missing a column
     df1 = pd.DataFrame(
         [
             ["chr1", 11],
@@ -318,7 +339,7 @@ def test_is_bedframe():
     )
     assert not is_bedframe(df1)
 
-    ### end column has invalid dtype
+    #   end column has invalid dtype
     df1 = pd.DataFrame(
         [
             ["chr1", 10, "20"],
@@ -328,7 +349,7 @@ def test_is_bedframe():
     )
     assert not is_bedframe(df1)
 
-    ### second interval start > ends.
+    #   second interval start > ends.
     df1 = pd.DataFrame(
         [
             ["chr1", 10, 20],
@@ -338,7 +359,7 @@ def test_is_bedframe():
     )
     assert not is_bedframe(df1)
 
-    ### third interval has a null in one column
+    #   third interval has a null in one column
     df1 = pd.DataFrame(
         [
             ["chr1", 10, 20, "first"],
@@ -355,7 +376,7 @@ def test_is_bedframe():
     with pytest.raises(ValueError):
         is_bedframe(df1, raise_errors=True)
 
-    ### first interval is completely NA
+    # first interval is completely NA
     df1 = pd.DataFrame(
         [
             [pd.NA, pd.NA, pd.NA, "first"],
@@ -440,7 +461,7 @@ def test_is_sorted():
         view_df, view_df=view_df, view_name_col="fruit", df_view_col="fruit"
     )
 
-    ## testing for non-standard column names
+    # testing for non-standard column names
     view_df = pd.DataFrame(
         [
             ["chrX", 1, 8, "oranges"],
@@ -451,9 +472,14 @@ def test_is_sorted():
     )
 
     assert is_sorted(
-        view_df, view_df=view_df, view_name_col="FRUIT", df_view_col="FRUIT", cols=["CHROM", "START", "END"], cols_view=["CHROM", "START", "END"]
+        view_df,
+        view_df=view_df,
+        view_name_col="FRUIT",
+        df_view_col="FRUIT",
+        cols=["CHROM", "START", "END"],
+        cols_view=["CHROM", "START", "END"]
     )
-    
+
     with pytest.raises(ValueError):
         # cols and view_cols are not passed as an arguments
         is_sorted(view_df, view_df=view_df)
@@ -484,8 +510,3 @@ def test_is_sorted():
 
     # view_df specifies a different ordering, so should not be sorted
     assert not is_sorted(bfs)
-
-if __name__ == "__main__":
-    test_is_covering()
-    test_is_tiling()
-    test_is_sorted()
