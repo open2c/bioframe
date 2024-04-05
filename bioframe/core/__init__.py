b17f3b1ab6ab0c7056a16dedf05b5c95afab1b45
Nezar Abdennur <nabdennur@gmail.com>
2024-04-05 19:57:45 -0400

    build: Use importlib.metadata for versioning (#195)
    * build: Use importlib.metadata for versioning
    
    * docs: Use importlib.metadata for version

--- bioframe/core/__init__.py
+++ bioframe/core/__init__.py
@@ -1,21 +1,50 @@
 from . import arrops
-
-from . import specs
-from .specs import *
-
-from . import stringops
-from .stringops import *
-
-from . import checks
-from .checks import *
-
-from . import construction
-from .construction import *
+from .checks import (
+    is_bedframe,
+    is_cataloged,
+    is_contained,
+    is_covering,
+    is_overlapping,
+    is_sorted,
+    is_tiling,
+    is_viewframe,
+)
+from .construction import (
+    from_any,
+    from_dict,
+    from_list,
+    from_series,
+    make_viewframe,
+    sanitize_bedframe,
+)
+from .specs import is_chrom_dtype, update_default_colnames
+from .stringops import (
+    is_complete_ucsc_string,
+    parse_region,
+    parse_region_string,
+    to_ucsc_string,
+)
 
 __all__ = [
     "arrops",
-    *specs.__all__,
-    *stringops.__all__,
-    *checks.__all__,
-    *construction.__all__,
+    "is_bedframe",
+    "is_cataloged",
+    "is_contained",
+    "is_covering",
+    "is_overlapping",
+    "is_sorted",
+    "is_tiling",
+    "is_viewframe",
+    "from_any",
+    "from_dict",
+    "from_list",
+    "from_series",
+    "make_viewframe",
+    "sanitize_bedframe",
+    "is_chrom_dtype",
+    "update_default_colnames",
+    "is_complete_ucsc_string",
+    "parse_region",
+    "parse_region_string",
+    "to_ucsc_string",
 ]
